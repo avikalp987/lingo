2d814bf5bcf36cfd64d3afae4d0f11e8439c6960
vikalp <avikalp233@gmail.com>
2024-04-06 19:23:36 +0530

    Sidebar

---
+++ components/sidebar-item.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { usePathname } from "next/navigation"
+import { Button } from "./ui/button"
+import Link from "next/link"
+import Image from "next/image"
+
+type Props = {
+    label: string,
+    iconSrc: string,
+    href: string,
+}
+
+export const SidebarItem = ({
+    label,
+    iconSrc,
+    href
+}: Props) => {
+
+    const pathname = usePathname()
+    const isActive = pathname===href
+
+    return (
+        <Button
+            variant={isActive ? "sidebarOutline" : "sidebar"}
+            className="justify-start h-[52px]"
+            asChild
+        >
+            <Link href={href}>
+                <Image 
+                    src={iconSrc}
+                    alt="label"
+                    className="mr-5"
+                    height={32}
+                    width={32}
+                />
+                {label}
+            </Link>
+        </Button>
+    )
+}
