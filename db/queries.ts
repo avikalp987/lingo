0ed4a26511df9fdc4ee4254006478f0bb324eeec
vikalp <avikalp233@gmail.com>
2024-04-07 19:26:43 +0530

    Courses Page

---
+++ db/queries.ts
@@ -0,0 +1,8 @@
+import { cache } from "react";
+import db from "./drizzle";
+
+export const getCourses = cache(async () => {
+    const data = await db.query.courses.findMany();
+
+    return data
+})
